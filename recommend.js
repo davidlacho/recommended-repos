5fbed45acc78f0f970d35fbac2924f9545d2bdc1
David Lacho <david.lacho@gmail.com>
2018-10-30 17:33:33 -0700

    rewrote functions as promises

--- recommend.js
+++ recommend.js
@@ -1,5 +1,4 @@
 const request = require('request');
-const fs = require('fs');
 const env = require('dotenv').config();
 
 if (env.error) {
@@ -10,98 +9,85 @@ const gitHubKey = process.env.GITHUB_KEY;
 const owner = process.argv[2];
 const repo = process.argv[3];
 
-
-/**
- * [downloadImageByURL fetches the desired avatar_url
- * and saves this information to the given filePath]
- * @param {[string]}  url   [A remote image URL to fetch]
- * @param {[string]}  filePath [A local path for where to persist the file]
- * @return {[undefined]} [undefined]
- */
-const downloadImageByURL = (url, filePath) => {
-  const startOfuserId = url.lastIndexOf('/u/');
-  const endOfUserId = url.lastIndexOf('?');
-  const userId = url.slice(startOfuserId + 2, endOfUserId);
-  request.get(url)
-    .on('error', (err) => {
-      if (err) {
-        throw new Error(`Error getting results from ${url}: ${err}`);
-      }
-    })
-    .on('response', (res, err) => {
-      if (err) {
-        throw new Error(`Something went wrong when receiving response from server: ${err}`);
-      }
-    })
-    .pipe(fs.createWriteStream(`${filePath}${userId}.jpg`, (err) => {
-      if (err) {
-        throw new Error('Problem writing file to path.');
-      }
-    }));
-  return undefined;
-};
-
-/**
- * [Callback will be passed in to getRepoContributors()
- * & will be executed when there is a response from request.
- * b loops through each item in the array:
- * It constructs a file path using the login value (e.g., "avatars/dhh.jpg")
- * It then passes the avatar_url value and the file path to downloadImageByURL]
- * @param  {[type]}   err     [error parameter with http request]
- * @param  {[type]}   results [incoming data from the server]
- * @return {[undefined]} [undefined]
- */
-const callback = (err, results) => {
-  if (err) {
-    throw err;
-  }
-  const parsedResults = JSON.parse(results);
-  const numberOfAvatars = parsedResults.length;
-  if (numberOfAvatars) {
-    console.log(`Downloading ${numberOfAvatars} user avatars.`);
-    parsedResults.forEach((result, i) => {
-      console.log(`Downloading ${i + 1}/${numberOfAvatars} user avatars.`);
-      downloadImageByURL(result.avatar_url, './avatars/');
-    });
-    console.log('Done!');
-  } else {
-    throw new Error('Could not find avatars to download. Please check user and repository exist.');
-  }
-  return undefined;
-};
-
-/**
- * [getRepoContributors makes a request for JSON, getting back an array of contributors,
- *  passes this data to cb, an anonymous callback function that it is given,
- *  ]
- * @param  {[type]}   repoOwner [the username of the repoowner]
- * @param  {[type]}   repoName  [the name of the repo]
- * @param  {Function} cb        [the callback function that will handle the data]
- * @return {[undefined]} [undefined]
- */
-const getRepoContributors = (repoOwner, repoName, cb) => {
-  const options = {
+const recommendRepos = (repoOwner, repoName) => new Promise((resolve) => {
+  resolve({
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'node application',
       Authorization: `token ${gitHubKey}`,
     },
-  };
-  request(options, (err, res, body) => {
-    cb(err, body);
   });
-  return undefined;
-};
+});
+
+const makeCall = options => new Promise((resolve, reject) => request(options, (err, res, body) => {
+  if (err) {
+    reject(err);
+  } else {
+    resolve(body);
+  }
+}));
+
+const parseJSON = body => new Promise((resolve, reject) => {
+  let parsedData;
+  try {
+    parsedData = JSON.parse(body);
+  } catch (e) {
+    reject(e);
+  }
+  resolve(parsedData);
+});
+
+const mapUsers = parseddata => new Promise((resolve, reject) => {
+  try {
+    resolve(parseddata.map(user => user.login));
+  } catch (e) {
+    if (e) {
+      reject(e);
+    }
+  }
+});
+
+// const getStarred = userArray => new Promise((resolve) => {
+//   userArray.forEach((user, i) => {
+//     const options = {
+//       url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+//       headers: {
+//         'User-Agent': 'node application',
+//         Authorization: `token ${gitHubKey}`,
+//       },
+//     };
+//     makeCall(options)
+//     .then()
+//   });
+//
+//
+// });
+
+const logItOut = item => new Promise((resolve, reject) => {
+  try {
+    console.log(item);
+  } catch (e) {
+    if (e) {
+      reject(e);
+    }
+  }
+});
 
 try {
-  // Does check to ensure arguments passed into command line:
   if (owner && repo && (process.argv.length === 4)) {
-    console.log('Welcome to the GitHub Avatar Downloader!');
-    getRepoContributors(owner, repo, callback);
+    console.log('Welcome to the GitHub Repo Recommender!');
+    recommendRepos(owner, repo)
+      .then(makeCall)
+      .then(parseJSON)
+      .then(mapUsers)
+      .then(logItOut)
+      // .then(getStarred)
+      .catch((e) => {
+        console.error(e);
+      });
   } else {
-    throw new Error('Missing required arguments: node download_avatars.js <owner> <repo>');
+    throw new Error('Missing required arguments: node recommend.js <owner> <repo>');
   }
 } catch (err) {
-  console.log(err);
-  process.exit();
+  console.error(err);
 }
